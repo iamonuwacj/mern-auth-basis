49114b3e66c4a781ab3d0070c9426784ad246bcf
iamonuwacj <onuwachinedu66@gmail.com>
2025-01-03 03:44:39 +0100

    modified the database connect string

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const ConnectDB = async () => {
     try {
         console.log("Mongo_Uri:", process.env.MONGO_ATLAS);
-        const conn = await mongoose.connect(process.env.MONGO_ATLAS)
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected ${conn.connection.host}`);
         
     } catch (error) {
